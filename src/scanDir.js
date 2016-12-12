f1e7a07d60dfac242df2b088f72d8c68a7a05cfd
coderwin <chenjiancj2011@outlook.com>
2016-12-12 17:50:37 +0800

    fix scanDir

--- src/scanDir.js
+++ src/scanDir.js
@@ -5,12 +5,18 @@ let fs = require('fs')
  * @type {Array}
  */
 let files = []
-
+/**
+ * 查找文件夹下指定的文件
+ * @param path 需要查找上午文件目录
+ * @param name 需要查找的文件名称
+ * @param file 附加参数，递归时用于匹配需要查找的文件
+ * @returns {Array}
+ */
 module.exports = function find(path, name, file) {
   /**
    * 忽略的文件夹
    * node_modules
-   * .git
+   * .git...
    */
   if(['.git','.idea','dist','test', 'node_modules'].indexOf(file)=='-1'){
     /**
